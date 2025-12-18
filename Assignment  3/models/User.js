4c05f00b85d35ee77bc3064057e6c885a69e26c3
Nida <nida43249@gmail.com>
2025-12-18 18:32:11 +0500

    Remove LAB task 3 and Assignment 3 from repo (keep local copies)

---
+++ Assignment  3/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  accountNumber: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  balance: {
+    type: Number,
+    default: 0
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
